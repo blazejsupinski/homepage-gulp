32b1d35c6394e83b64750eaf68a05de83d4d7394
Błażej Supiński <berekeks@gmail.com>
2019-07-29 21:22:57 +0200

    zmiana tla

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
-"use strict";
+const tesc = document.querySelector('.section__six--js');
+tesc.innerHTML = "Java Script != Java";
 
-console.log(`Hello world!`);
+
+function nazwaFunkcji (imie, wiek){
+    console.log(`Witaj ${imie} lat ${wiek}`)
+
+}
+
+nazwaFunkcji('blazj',37);
+
+const pstryczek = document.querySelector('.button__hamburger');
+pstryczek.addEventListener('click', (e) => {
+    const nawigacja = document.querySelector('.ulList');
+    nawigacja.classList.toggle('visible')
+})
